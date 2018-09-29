e88ff49afc3803433e3c55314f47e3e050129f2b
PayungsakCNR <payungsak.k@cnr.psu.ac.th>
2018-09-29 14:15:40 +0700

    Update database.js

--- configs/database.js
+++ configs/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'url': 'mongodb://webapp:webapp1234@127.0.0.1:27017/web-app-proj' // looks like mongodb://<user>:<pass>@host:port/DBName
+    'url': 'mongodb://webapp:webapp1234@127.0.0.1:27017/web-app-proj'
 };
