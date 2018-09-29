d1520099743952b4ba9389bdcfb75735cd3e3ca0
PayungsakCNR <payungsak.k@cnr.psu.ac.th>
2018-09-29 14:20:00 +0700

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,36 +1,35 @@
 module.exports = function (app, passport) {
 
-    //หน้าหลัก
+    //Render Homepage
     app.get('/', function (req, res) {
         res.render('index.ejs'); 
     });
-    //หน้าเข้าสู่ระบบ
+    //Render Login Page
     app.get('/login', function (req, res) {
         res.render('login.ejs', { message: req.flash('loginMessage') });
     });
 
-    //ตัวจัดการเข้าสู่ระบบ
+    //Login management
     app.post('/login', passport.authenticate('local-login', {
         successRedirect: '/profile', // redirect to the secure profile section
         failureRedirect: '/login', // redirect back to the signup page if there is an error
         failureFlash: true // allow flash messages
     }));
 
-    //หน้าสมัครสมาชิก
+    //Render Sign Up Page
     app.get('/signup', function (req, res) {
         res.render('signup.ejs', { message: req.flash('signupMessage') });
     });
 
-    // ตัวจัดการการสมัครสมาชิก
+    // Sign up management
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect: '/profile', //สมัครสมาชิกสำเร็จ
-        failureRedirect: '/signup', //ถ้าสมัครสมาชิกไม่สำเร็จ
+        successRedirect: '/profile', //Successfully
+        failureRedirect: '/signup', //Not Successfully
         failureFlash: true 
     }));
 
     //If logined successfully , check user role for render site
     app.get('/profile', isLoggedIn, function (req, res) {
-        //console.log(isAdmin(req, res,next));
         if(req.user.local.role == 'ADMIN'){
             res.render('profile-dev-new.ejs', {
                 user: req.user 
